c817e2aea9f007b7f539b14df60a1c5d385f03f8
Kardun2203 <75862501+Kardun2203@users.noreply.github.com>
2020-12-13 00:56:09 +0700

    Add files via upload

---
+++ idk.js
@@ -0,0 +1,42 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVER] Server Started!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
+       
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block! Mo di unblock? Dm admin : @alvarobhermann_')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+create('BarBar', options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
